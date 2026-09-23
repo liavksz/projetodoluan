09f081f9e3548b8c1cfc74bb598b281071a38425
ALICIA SIQUEIRA SOUZA <alicia_s_souza@estudante.sesisenai.org.br>
2026-09-23 16:59:26 -0300

    Meu primeiro commit - Criando o Header

---
+++ projeto/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header"
+
+function App(){
+  return(
+    <>
+    <Header></Header>
+    {/* <Main></Main>
+    <Footer></Footer>*/}
+    </>
+  )
+}
+
+export default App
